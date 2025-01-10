b3438e6d60c1549371ea4da6aa5db5e6800eb560
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2025-01-10 11:52:21 +0530

    models created

---
+++ models/task.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Task = sequelize.define("Task", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  status: {
+    type: DataTypes.ENUM("TODO", "IN_PROGRESS", "DONE"),
+    defaultValue: "TODO",
+  },
+});
+
+module.exports = Task;
