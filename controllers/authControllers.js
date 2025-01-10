9f4e3b544e260d0fd79ae773a7a13420b8cfc3d1
Shubham-S-Nimje <shubhamsnimje@gmail.com>
2025-01-10 12:53:21 +0530

    log in routes

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,12 +1,15 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
+//generate token
 const generateToken = (userDetails) => {
   return jwt.sign(userDetails, process.env.JWT_SECRET, {
     expiresIn: "24h",
   });
 };
 
+//user registration
 exports.register = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -22,22 +25,68 @@ exports.register = async (req, res) => {
       return res.status(400).json({ error: "Email already registered" });
     }
 
+    // hashing password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const user = await User.create({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
+    // console.log(user);
 
     const token = generateToken({
       userId: user.userId,
       name: user.name,
       email: user.email,
     });
+    // console.log(token);
 
     res.status(201).json({
       message: "User registered successfully",
       user: {
         userId: user.userId,
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+//user login
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(404)
+        .json({ error: "Email, and password are required fields." });
+    }
+
+    const user = await User.findOne({ where: { email } });
+    // console.log(user);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    await user.update({ lastLogin: new Date() });
+
+    const token = generateToken({ userId: user.userId });
+    // console.log(token);
+
+    res.json({
+      message: "Login successful",
+      user: {
         name: user.name,
         email: user.email,
       },
